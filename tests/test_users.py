4c2e75108d485ccfd1b1aefe85b3a57bb8b4e001
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2026-06-10 01:22:25 -0700

    force test failure

--- tests/test_users.py
+++ tests/test_users.py
@@ -12,7 +12,7 @@ def test_create_user(client):
         "last_name": "Lovelace",
         "email": "ada@example.com",
     })
-    assert response.status_code == 201
+    assert response.status_code == 999
     body = response.get_json()
     assert body["first_name"] == "Ada"
     assert body["last_name"] == "Lovelace"
